48b34a8863f7ae5bf253e802a1aec5e18a410255
chomusuke-png <owokenji@gmail.com>
2025-12-22 16:21:33 -0300

    selector de ancho mejorado

--- includes/class-afp-builder-ui.php
+++ includes/class-afp-builder-ui.php
@@ -129,14 +129,35 @@ class AFP_Builder_UI {
                     <label>Max:<input type="number" name="afp_fields[<?php echo $index; ?>][max_value]" value="<?php echo esc_attr($max_val); ?>" class="widefat" style="width:80px;"></label>
                 </div>
                 <div class="afp-form-row afp-flex afp-settings-row" style="<?php echo $hide_req ? 'display:none;' : ''; ?>">
-                    <label><input type="checkbox" name="afp_fields[<?php echo $index; ?>][required]" value="1" <?php checked($req, 1); ?>> Obligatorio</label>
-                    <label style="margin-left:20px; display:<?php echo $hide_width ? 'none' : 'block'; ?>">Ancho:
-                        <select name="afp_fields[<?php echo $index; ?>][width]">
-                            <option value="100" <?php selected($width, '100'); ?>>100%</option>
-                            <option value="50" <?php selected($width, '50'); ?>>50%</option>
-                            <option value="33" <?php selected($width, '33'); ?>>33%</option>
-                        </select>
+                    <label>
+                        <input type="checkbox" name="afp_fields[<?php echo $index; ?>][required]" value="1" <?php checked($req, 1); ?>> 
+                        Obligatorio
                     </label>
+
+                    <div class="afp-width-control" style="display:<?php echo $hide_width ? 'none' : 'flex'; ?>">
+                        <span class="afp-control-label">Ancho:</span>
+                        <div class="afp-width-options">
+                            <?php 
+                            // Definimos las opciones y sus clases visuales
+                            $width_opts = [
+                                '100' => ['label' => 'Completo', 'class' => 'afp-w-100'],
+                                '50'  => ['label' => 'Mitad',    'class' => 'afp-w-50'],
+                                '33'  => ['label' => 'Tercio',   'class' => 'afp-w-33'],
+                            ];
+                            
+                            foreach ($width_opts as $val => $opt):
+                                $is_checked = ($width == $val) ? 'checked' : '';
+                            ?>
+                                <label class="afp-width-btn" title="<?php echo esc_attr($opt['label']); ?>">
+                                    <input type="radio" 
+                                           name="afp_fields[<?php echo $index; ?>][width]" 
+                                           value="<?php echo esc_attr($val); ?>" 
+                                           <?php echo $is_checked; ?>>
+                                    <span class="afp-width-icon <?php echo esc_attr($opt['class']); ?>"></span>
+                                </label>
+                            <?php endforeach; ?>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
